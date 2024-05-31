e3015419354373ec162a08221ea960caf5df2848
Mirko Abozzi <mirko.abozzi@gmail.com>
2024-05-31 23:56:47 +0200

    scroll event

--- base_di_partenza/assets/js/index.js
+++ base_di_partenza/assets/js/index.js
@@ -1,4 +1,16 @@
 const button = document.getElementById("getButton");
-const navBar = document.querySelector("nav");
+const navBar = document.getElementById("getNav");
+const buttons = document.querySelectorAll("whiteButton");
 console.log(button);
 console.log(navBar);
+console.log(buttons);
+
+window.addEventListener("scroll", function () {
+  if (window.scrollY > 250) {
+    button.style.backgroundColor = "#1a8917";
+    navBar.style.backgroundColor = "white";
+  } else {
+    navBar.style.backgroundColor = "#ffc017";
+    button.style.backgroundColor = "#191919";
+  }
+});
